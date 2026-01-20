e5d7848c3298ef7217bcc34ac020fc118a09fbb2
Prem Prasad <premrinkinoniya@gmail.com>
2026-01-21 00:07:23 +0530

    init commit

---
+++ src/MovesZero.java
@@ -0,0 +1,28 @@
+//optimal Solution
+public class MovesZero {
+    public static  void main(String[] args){
+        int[] arr={1,2,0,5,8,0,0,5,6};
+        swap(arr);
+
+    }
+    static  void swap(int [] arr){
+        int j=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        for(int i=j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+}
