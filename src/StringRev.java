2d0dc5b9f70773c9228568137b4ef56440de4717
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-16 17:47:52 +0530

    init commit

---
+++ src/StringRev.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StringRev {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String string=sc.nextLine();
+        System.out.println(rev(string));
+    }
+    static String rev(String string){
+        char[] arr=string.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while (i<j){
+            char temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        String rev=new String(arr);
+        return rev;
+    }
+}
